9148ea62aef25acbeea62e5fa9d1b29df2eee85d
gejpark <122942803+gejpark@users.noreply.github.com>
2023-05-08 19:05:25 -0700

    set grayscale test
    example used for reference: https://labs.phaser.io/view.html?src=src/renderer/grayscale%20pipeline%20added%20locally.js

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -1,28 +1,26 @@
-const fragShader = `
-#define SHADER_NAME GRAYSCALE
-precision mediump float;
-uniform sampler2D uMainSampler[%count%];
-uniform float gray;
-varying vec2 outTexCoord;
-varying float outTexId;
-varying vec4 outTint;
-varying vec2 fragCoord;
-void main()
-{
-    vec4 texture;
-    %forloop%
-    gl_FragColor = texture;
-    gl_FragColor.rgb = mix(gl_FragColor.rgb, vec3(0.2126 * gl_FragColor.r + 0.7152 * gl_FragColor.g + 0.0722 * gl_FragColor.b), gray);
-}
-`;
-
 class GrayScalePipeline extends Phaser.Renderer.WebGL.Pipelines.MultiPipeline
 {
     constructor (game)
     {
         super({
             game,
-            fragShader,
+            fragShader: `
+            #define SHADER_NAME GRAYSCALE
+            precision mediump float;
+            uniform sampler2D uMainSampler[%count%];
+            uniform float gray;
+            varying vec2 outTexCoord;
+            varying float outTexId;
+            varying vec4 outTint;
+            varying vec2 fragCoord;
+            void main()
+            {
+                vec4 texture;
+                %forloop%
+                gl_FragColor = texture;
+                gl_FragColor.rgb = mix(gl_FragColor.rgb, vec3(0.2126 * gl_FragColor.r + 0.7152 * gl_FragColor.g + 0.0722 * gl_FragColor.b), gray);
+            }
+            `,
             uniforms: [
                 'uProjectionMatrix',
                 'uMainSampler',
@@ -33,12 +31,12 @@ class GrayScalePipeline extends Phaser.Renderer.WebGL.Pipelines.MultiPipeline
         this._gray = 1;
     }
 
-    onPreRender ()
+    onPreRender () //on step before rendering effect.
     {
-        this.set1f('gray', this._gray);
+        this.set1f('gray', this._gray); //applies value to uniform
     }
 
-    get gray ()
+    get gray () //get and set values
     {
         return this._gray;
     }
@@ -62,14 +60,22 @@ class Play extends Phaser.Scene {
     }
 
     create() {
-        const grayscalePipeline = this.renderer.pipelines.add('Gray', new GrayScalePipeline(this.game));
+        this.test =  new GrayScalePipeline(this.game);
+        var grayscalePipeline = this.renderer.pipelines.add('Gray', this.test);
         this.background1 = this.add.tileSprite(0, 100, 640, 480, 'scrolling_tile').setOrigin(0, 0).setPipeline(grayscalePipeline);    //All background sprites are used for parallax scrolling effect.
-        
-
+        this.temp = 0.5;
+        this.multiplier = 1;
     }
 
     update() {
+        var grayscalePipeline = this.renderer.pipelines.add('Gray', this.test);
+        grayscalePipeline.gray = this.temp;
+        this.temp += 0.01 * this.multiplier;
+        if (this.temp <= 0 || this.temp >= 1) {
+            this.multiplier *= -1;
+        }
         this.background1.tilePositionY -= 2;
+        // grayscalePipeline.gray = 1;
         
     }
 }
