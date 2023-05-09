98edbcb8cb703dceb8d697bbc4e91551f8199427
gejpark <122942803+gejpark@users.noreply.github.com>
2023-05-08 18:49:45 -0700

    got some type of shader pipeline to work
    https://labs.phaser.io/view.html?src=src/renderer/grayscale%20pipeline%20added%20locally.js

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -1,49 +1,75 @@
+const fragShader = `
+#define SHADER_NAME GRAYSCALE
+precision mediump float;
+uniform sampler2D uMainSampler[%count%];
+uniform float gray;
+varying vec2 outTexCoord;
+varying float outTexId;
+varying vec4 outTint;
+varying vec2 fragCoord;
+void main()
+{
+    vec4 texture;
+    %forloop%
+    gl_FragColor = texture;
+    gl_FragColor.rgb = mix(gl_FragColor.rgb, vec3(0.2126 * gl_FragColor.r + 0.7152 * gl_FragColor.g + 0.0722 * gl_FragColor.b), gray);
+}
+`;
+
+class GrayScalePipeline extends Phaser.Renderer.WebGL.Pipelines.MultiPipeline
+{
+    constructor (game)
+    {
+        super({
+            game,
+            fragShader,
+            uniforms: [
+                'uProjectionMatrix',
+                'uMainSampler',
+                'gray'
+            ]
+        });
+
+        this._gray = 1;
+    }
+
+    onPreRender ()
+    {
+        this.set1f('gray', this._gray);
+    }
+
+    get gray ()
+    {
+        return this._gray;
+    }
+
+    set gray (value)
+    {
+        this._gray = value;
+    }
+}
+
+
 class Play extends Phaser.Scene {
     constructor() {
         super('playScene');
+
     }
 
     preload() {
-        this.load.glsl('wave', 'src/shader/shader1.frag');
         this.load.image('background', './assets/Sprites/background_sprite.png');
         this.load.image('scrolling_tile', './assets/Sprites/scrolling_tile.png');
     }
 
     create() {
+        const grayscalePipeline = this.renderer.pipelines.add('Gray', new GrayScalePipeline(this.game));
+        this.background1 = this.add.tileSprite(0, 100, 640, 480, 'scrolling_tile').setOrigin(0, 0).setPipeline(grayscalePipeline);    //All background sprites are used for parallax scrolling effect.
         
-        this.add.image(0, 0, 'background').setOrigin(0, 0);
-        this.background1 = this.add.tileSprite(0, 0, 640, 480, 'scrolling_tile', 'wave').setOrigin(0,0);
-        //Warp tilesprite for "Mode-7" effect
-        // this.background1.SetAngle(30);
-        // this.add.rectangle(0, 0, game.config.width, borderUISize * 2 + borderUISize + borderPadding, 0x997577).setOrigin(0, 0);
-        // console.log(this.background1.renderer.projectionMatrix);
-        // const shader = this.add.shader('tunnel', 400, 300, 800, 600, [ this.background1 ]);
-        const shader = this.make.shader({
-            key: 'wave',
-            x: config.width/2,
-            y: config.height/2,
-            width: config.width,
-            height: config.height,
-            add: false
-        });
 
-        //  Make a Bitmap Mask from it
-        const mask = shader.createBitmapMask();
-
-        //  Apply the mask to this image
-        // this.add.image(400, 300, 'scrolling_tile').setMask(mask);
-        this.background1.setMask(mask);
-
-        // this.background1 = this.add.tileSprite(0, 0, 640, 480, 'scrolling_tile').setOrigin(0,0);
-        // this.background1.setMask(mask);
-
-
-
-        
-        
     }
 
     update() {
         this.background1.tilePositionY -= 2;
+        
     }
 }
