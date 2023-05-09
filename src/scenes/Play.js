513a7934b48b6664e4041b8a864448d5c867e1db
gejpark <122942803+gejpark@users.noreply.github.com>
2023-05-08 22:51:31 -0700

    figured out how to just redraw image using shaders (texture2d)
    Check lines #53 to #54
    Notes:
    - https://computergraphics.stackexchange.com/questions/5724/glsl-can-someone-explain-why-gl-fragcoord-xy-screensize-is-performed-and-for
    - https://stackoverflow.com/questions/35604130/convert-gl-fragcoord-coordinate-to-screen-positions

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -1,5 +1,5 @@
 
-//Code for fragShader taken from: https://www.shadertoy.com/view/Wt33Wf#
+//Code for fragShader taken from: https://www.shadertoy.com/view/dlt3zs
 class GrayScalePipeline extends Phaser.Renderer.WebGL.Pipelines.MultiPipeline
 {
     constructor (game)
@@ -10,59 +10,53 @@ class GrayScalePipeline extends Phaser.Renderer.WebGL.Pipelines.MultiPipeline
             #ifdef GL_ES
             precision mediump float;            //accuracy
             uniform float time;                 //time delta
-            // Enable blending
-            // glEnable(gl.BLEND);
-            // gl.blendFunc(gl.SRC_COLOR, gl.DST_COLOR);
+            uniform sampler2D ColorTexture;
             #endif
 
-            float grid(vec2 uv, float battery) {
-                vec2 size = vec2(uv.y, uv.y * uv.y * 0.2) * 0.01;
-                // uv += vec2(0.0, time * 4.0 * (battery + 0.05));
-                uv += vec2(0.0, time * 4.0 );                                       //set speed of grid movement
-                uv = abs(fract(uv) - 0.5);
-                vec2 lines = smoothstep(size, vec2(0.0), uv);                       //draw lines
-                lines += smoothstep(size * 5.0, vec2(0.0), uv) * 0.4 * battery;     //draw a smooth line from 0,0 to end of uv
-                return clamp(lines.x + lines.y, 0.0, 3.0);                          //return gridval???
+            float Quant(float cc,float steps) {
+                return floor(steps*cc)/steps;           // pure quantization
             }
 
-            void main( )
-            {
-                // Enable blending
-                // glEnable(gl.BLEND);
-                //glblendFunc(gl.SRC_COLOR, gl.DST_COLOR);
-                vec2 resolution = vec2(640,480);
-                vec2 uv = (2.0 * gl_FragCoord.xy - resolution.xy)/resolution.y;     //?not sure what this is?
-                float battery = 0.1;                                                //affects speed of scroll
-                {
-                    // Grid
-                    // float fog = smoothstep(0.1, -0.02, abs(uv.y + 0.2));         //add fog if needed
-                    // vec3 col = vec3(0.0, 0.1, 0.2);                                 //color to add (to gl_FragColor)
-                    vec3 col = vec3(1.0, 1.0, 1.0);
-                    if (uv.y < -0.2) {
-                        uv.y = 3.0 / (abs(uv.y + 0.2) + 0.05);
-                        uv.x *= uv.y * 1.0;
-                        float gridVal = grid(uv, battery);
-                        // col = mix(col, vec3(1.0, 0.5, 1.0), gridVal);
-                        col = mix(col, vec3(0.0, 0.0, 0.0), gridVal/3.0);
-                    }
-
-                    // col += fog * fog * fog;
-                    // col = mix(vec3(col.r, col.r, col.r) * 0.5, col, battery * 0.7);
-
-                    if (col.r >= 0.80) {
-                        gl_FragColor = vec4(col,0.0);    
-                    } else {
-                        gl_FragColor = vec4(col,1.0);
-                    }
-                    // gl_FragColor = vec4(col,1.0);
-                    // gl_FragColor.a = 0.0;
-                }
-
+            vec2 CycleGrid(vec2 uv,float colorsteps) {
+                float fade = (abs(uv.y)-.1)/.9;
+                fade = 1.0-step(fade,0.085);
+                vec2 v = vec2(uv.x*abs(1.0/uv.y),abs(1.0/uv.y));
+                vec2 fs =  fract(v+0.5)-0.5;
+                float e = length(vec2(abs(fs.x)-0.5,0.0));
                 
+                float lines = step(e,.05);
+                float fill = fs.y+0.5;
+                fill+=Quant(fract(time*0.5),colorsteps);// Cycle colours
+                fill = Quant(fract(fill),colorsteps);
+                return vec2(lines,(fill+0.1)) * fade;
+            }
+
+            void main() {
+                //Set resolution
+                vec2 resolution = vec2(640, 480);
+
+                // Pixel coordinates
+                vec2 uv = (2.0*gl_FragCoord.xy-resolution.xy)/resolution.y;
+
+                vec2 dat = CycleGrid(uv , 12.0);  // get grid with 12 colour steps
+                vec3 col = vec3(dat.y,0.0,0.0);
+                // col.rgb += dat.x;              //white lines
+
+                // Output to screen
+
+                // vec4 textureCol = texture2D(ColorTexture, gl_FragCoord.xy);
+                // gl_FragColor = vec4(col, dat.y);
+                // gl_FragColor.rgb = mix(col, vec3(textureCol.r, textureCol.g, textureCol.b), 100.0);
+                
+                // gl_FragColor = vec4(col, 1.0);
+
+                //To just redraw background image:
+                gl_FragColor = texture2D(ColorTexture, vec2(gl_FragCoord.x, resolution.y - gl_FragCoord.y)/resolution);
             }
             `,
             uniforms: [
-                'time'
+                'time',
+                'ColorTexture'
             ]
         });
 
@@ -109,16 +103,18 @@ class Play extends Phaser.Scene {
         this.test = new GrayScalePipeline(this.game);
         // var grayscalePipeline = this.renderer.pipelines.add('Gray', this.test);
         const  grayscalePipeline = this.renderer.pipelines.add('Gray', this.test);
-        // this.background1 = this.add.image(0, 0, 'background').setOrigin(0, 0).setPipeline(grayscalePipeline);
         this.background1 = this.add.image(0, 0, 'background').setOrigin(0, 0);
         this.background1.setPipeline(grayscalePipeline);
+        // this.tile = this.add.image(0,0, 'scrolling_tile').setOrigin(0,0);
+        // this.tile.setPipeline(grayscalePipeline);
         this.temp = 0.5;
         this.multiplier = 1;
     }
 
     update() {
-        // const grayscalePipeline = this.renderer.pipelines.add('Gray', this.test);
         const grayscalePipeline = this.renderer.pipelines.get('Gray');
+        // const mask = grayscalePipeline.createBitmapMask();
+        // this.background1.setMask(mask);
         grayscalePipeline.gray = this.temp;
         this.temp += 0.01;
         
