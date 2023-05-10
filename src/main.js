6079e7c683d3d2c11d0afeaff4ef119e69e93bb8
gejpark <122942803+gejpark@users.noreply.github.com>
2023-05-09 23:32:18 -0700

    press down to slow down

--- src/main.js
+++ src/main.js
@@ -43,7 +43,7 @@ let config = {
 }
 let game = new Phaser.Game(config);
 // reserve keyboard vars
-let KEY_SPACE, KEY_LEFT, KEY_RIGHT, KEY_UP;
+let KEY_SPACE, KEY_LEFT, KEY_RIGHT, KEY_UP, KEY_DOWN;
 // Set UI sizes
 let borderUISize = game.config.height / 15;
 let borderPadding = borderUISize / 3;
