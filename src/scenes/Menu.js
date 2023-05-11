12ccf3738dbb5e7b8b3997674a60fc2c11342742
gejpark <122942803+gejpark@users.noreply.github.com>
2023-05-10 17:32:36 -0700

    added animated menu background

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -9,9 +9,10 @@ class Menu extends Phaser.Scene {
 
         //load base image for spaceship revolving animation
         this.load.image('spaceship_revolve_base', './assets/Sprites/spaceship_revolve_base.png');
+        //load menu animation
+        this.load.spritesheet('menu_background_animated', './assets/Sprites/menu_background.png', {frameWidth: 640, frameHeight: 480, startFrame: 0, endFrame: 5});
         //load revolving spaceship spritesheet
         this.load.spritesheet('spaceship_revolve','./assets/Sprites/spaceship3-Sheet.png', {frameWidth: 90, frameHeight: 70, startFrame: 0, endFrame: 179});
-
     }
 
     create() {
@@ -23,7 +24,14 @@ class Menu extends Phaser.Scene {
             repeat: -1,
         });
 
+        this.anims.create({
+            key: 'menu_background_animated',
+            frames: this.anims.generateFrameNumbers('menu_background_animated', {start: 0, end: 5, first: 0}),
+            frameRate: 15,
+        })
+
         this.menu_background = this.add.sprite(0,0,'menu_background').setOrigin(0,0);
+        this.menu_background.anims.play('menu_background_animated', true);
 
         this.spaceship_revolve_base = this.add.sprite(0,0,'spaceship_revolve_base').setOrigin(0,0);
         this.spaceship_revolve_base.x = (game.config.width - this.spaceship_revolve_base.width)/2;
@@ -45,7 +53,7 @@ class Menu extends Phaser.Scene {
             },
             fixedWidth: 0
         }
-        this.add.text(game.config.width/2, game.config.height/4 - borderUISize - borderPadding, 'Press Right Key to Play', menuConfig).setOrigin(0.5);
+        // this.add.text(game.config.width/2, game.config.height/4 - borderUISize - borderPadding, 'Press Right Key to Play', menuConfig).setOrigin(0.5);
 
         KEY_LEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
         KEY_RIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
